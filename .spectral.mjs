07a4c91eb081f558ac4889c3e140083a5dd93e96
pullobestfriend@gmail.com <pullobestfriend@gmail.com>
2023-06-01 02:10:56 +0700

    Added 4 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIwMzY5Mg/spectral.js?branch_slug=main&token=54887316-e80e-420c-81bf-001433e55d51";
+export default { extends: ruleset };
